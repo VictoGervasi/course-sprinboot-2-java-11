5e54a0d68a80105d4e6ad05b53604a86966106a4
Victor Gervasi <vicgeragui@gmail.com>
2022-07-18 15:40:49 -0300

    Category

---
+++ src/main/java/com/aguisa/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.aguisa.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aguisa.course.entities.Category;
+import com.aguisa.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> optional = repository.findById(id);
+		return optional.get();
+	}
+}
