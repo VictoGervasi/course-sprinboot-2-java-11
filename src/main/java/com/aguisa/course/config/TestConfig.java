663ebefa0ffa093837c91331a3f9f2d3fd223e29
Victor Gervasi <vicgeragui@gmail.com>
2022-07-19 08:58:53 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/aguisa/course/config/TestConfig.java
+++ src/main/java/com/aguisa/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.aguisa.course.entities.Category;
 import com.aguisa.course.entities.Order;
+import com.aguisa.course.entities.OrderItem;
 import com.aguisa.course.entities.Product;
 import com.aguisa.course.entities.User;
 import com.aguisa.course.entities.enums.OrderStatus;
 import com.aguisa.course.repositories.CategoryRepository;
+import com.aguisa.course.repositories.OrderItemRepository;
 import com.aguisa.course.repositories.OrderRepository;
 import com.aguisa.course.repositories.ProductRepository;
 import com.aguisa.course.repositories.UserRepository;
@@ -33,6 +35,11 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -72,5 +79,12 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
