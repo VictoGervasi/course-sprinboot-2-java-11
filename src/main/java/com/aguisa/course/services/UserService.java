db7c759d7296c36900bedc4c8463a123bc7093de
Victor Gervasi <vicgeragui@gmail.com>
2022-07-19 10:37:41 -0300

    User insert

--- src/main/java/com/aguisa/course/services/UserService.java
+++ src/main/java/com/aguisa/course/services/UserService.java
@@ -23,4 +23,9 @@ public class UserService {
 		Optional<User> optional = repository.findById(id);
 		return optional.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+		
+	}
 }
