e201b289efee3c934a0115376aaca08db646d458
Victor Gervasi <vicgeragui@gmail.com>
2022-07-20 09:49:24 -0300

    Exception handling - delete

--- src/main/java/com/aguisa/course/services/UserService.java
+++ src/main/java/com/aguisa/course/services/UserService.java
@@ -4,34 +4,46 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.aguisa.course.entities.User;
 import com.aguisa.course.repositories.UserRepository;
+import com.aguisa.course.services.exxceptions.DatabaseException;
+import com.aguisa.course.services.exxceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> optional = repository.findById(id);
-		return optional.get();
+		return optional.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
-		return repository.save(obj);	
+		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
@@ -42,6 +54,6 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
